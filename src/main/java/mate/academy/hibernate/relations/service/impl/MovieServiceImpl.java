a5392941c273fcb45d555809a11e4d0417151327
Olexander Kucherenko <amarokthegrey@gmail.com>
2026-02-04 17:21:28 +0200

    removed unused import in MovieServiceImpl

--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,6 +1,5 @@
 package mate.academy.hibernate.relations.service.impl;
 
-import mate.academy.hibernate.relations.dao.ActorDao;
 import mate.academy.hibernate.relations.dao.MovieDao;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
